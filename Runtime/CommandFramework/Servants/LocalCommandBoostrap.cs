f1534e57e5daf03235acd0a5b99c90c81ee441a7
touky <touky@users.noreply.github.com>
2020-10-24 14:12:19 -0400

    bootstrapper

---
+++ Runtime/CommandFramework/Servants/LocalCommandBoostrap.cs
@@ -0,0 +1,9 @@
+﻿namespace Prateek.Runtime.CommandFramework.Servants
+{
+    using Prateek.Runtime.DaemonFramework.Servants;
+
+    public sealed class LocalCommandBoostrap
+        : ServantBootstrap<LocalCommandServant>
+    {
+    }
+}
