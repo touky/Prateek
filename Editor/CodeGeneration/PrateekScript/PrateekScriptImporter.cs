4e426ea35533ff1e19c8f9d073f9704d17c067cb
touky <touky@users.noreply.github.com>
2021-07-19 19:52:37 -0400

    importer deactivation

--- Editor/CodeGeneration/PrateekScript/PrateekScriptImporter.cs
+++ Editor/CodeGeneration/PrateekScript/PrateekScriptImporter.cs
@@ -3,17 +3,17 @@
     using Prateek.Editor.CodeGeneration.PrateekScript.RuntimeGeneration;
     using UnityEditor.Experimental.AssetImporters;
 
-    [ScriptedImporter(1, Glossary.importExtension)]
-    public class PrateekScriptImporter : ScriptedImporter
-    {
-        #region Class Methods
-        public override void OnImportAsset(AssetImportContext ctx)
-        {
-            var builder = PrateekScriptBuilder.GetInstance();
-            builder.AddFile(ctx.assetPath);
-            PrateekScriptBuilderEditorWindow.AddBuilder(builder);
-        }
-        #endregion
-    }
+    //todo fix [ScriptedImporter(1, Glossary.importExtension)]
+    //todo fix public class PrateekScriptImporter : ScriptedImporter
+    //todo fix {
+    //todo fix     #region Class Methods
+    //todo fix     public override void OnImportAsset(AssetImportContext ctx)
+    //todo fix     {
+    //todo fix         var builder = PrateekScriptBuilder.GetInstance();
+    //todo fix         builder.AddFile(ctx.assetPath);
+    //todo fix         PrateekScriptBuilderEditorWindow.AddBuilder(builder);
+    //todo fix     }
+    //todo fix     #endregion
+    //todo fix }
 }
 
